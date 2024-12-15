ba7b7aaa50ae72943c73bb212f7c972203eba558
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-12-15 12:26:23 +0100

    feat: Add unit tests for TrainJsonToSqlTransformer and LoadController

---
+++ INTERNAL_SOURCE_LOAD_TEST/LoadControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using INTERNAL_SOURCE_LOAD;
+using INTERNAL_SOURCE_LOAD.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework.Legacy;
+
+
+namespace INTERNAL_SOURCE_LOAD_TEST
+{
+    [TestFixture]
+    public class TrainJsonToSqlTransformerTests
+    {
+        private TrainJsonToSqlTransformer _transformer;
+        private LoadController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _transformer = new TrainJsonToSqlTransformer();
+            List<IJsonToSqlTransformer> transformers = new List<IJsonToSqlTransformer> { new TrainJsonToSqlTransformer() };
+            _controller = new LoadController(transformers);
+        }
+
+    }
+}   
