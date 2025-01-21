57ca8489eee1a5cececaede6005668bee4504648
NoahDelgado <noah.delgado1@eduvaud.ch>
2025-01-21 16:23:43 +0100

    refactor: Update Program.cs to use IJsonToModelTransformer and add SQL generation for TrainStation

--- INTERNAL-SOURCE-LOAD/Program.cs
+++ INTERNAL-SOURCE-LOAD/Program.cs
@@ -1,4 +1,5 @@
 using INTERNAL_SOURCE_LOAD;
+using INTERNAL_SOURCE_LOAD.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<IJsonToSqlTransformer, TrainJsonToSqlTransformer>();
-
+builder.Services.AddTransient(typeof(IJsonToModelTransformer<>), typeof(JsonToModelTransformer<>));
 
 var app = builder.Build();
 
@@ -27,3 +27,18 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+static string GenerateTrainStationSql(TrainStation trainStation)
+{
+    var sqlStatements = new List<string>();
+
+    foreach (var departure in trainStation.Departures)
+    {
+        string sql = $"INSERT INTO TrainDepartures (DepartureStationName, DestinationStationName, ViaStationNames, DepartureTime, TrainGroup, TrainLine, Platform, Sector) " +
+                     $"VALUES ('{departure.DepartureStationName}', '{departure.DestinationStationName}', '{string.Join(",", departure.ViaStationNames)}', '{departure.DepartureTime:yyyy-MM-dd HH:mm:ss}', " +
+                     $"'{departure.Train.G}', '{departure.Train.L}', '{departure.Platform}', {(departure.Sector != null ? $"'{departure.Sector}'" : "NULL")});";
+
+        sqlStatements.Add(sql);
+    }
+
+    return string.Join("\n", sqlStatements);
+}
