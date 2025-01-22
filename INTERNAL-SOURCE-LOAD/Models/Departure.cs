58a3908f618d589c9cfbaf220f604e2e709bdfa1
NoahDelgado <noah.delgado1eduvaud.ch>
2025-01-22 23:31:21 +0100

    feat: Add TableName attribute to Departure record for database mapping

--- INTERNAL-SOURCE-LOAD/Models/Departure.cs
+++ INTERNAL-SOURCE-LOAD/Models/Departure.cs
@@ -12,5 +12,6 @@ namespace INTERNAL_SOURCE_LOAD.Models
     /// <param name="Train">The train associated with the departure.</param>
     /// <param name="Platform">The platform from which the train departs.</param>
     /// <param name="Sector">The sector of the station where the train is located. Can be null if it's not specific.</param>
+    [TableName("Departures")]
     public record Departure(string DepartureStationName, string DestinationStationName, List<string> ViaStationNames, DateTime DepartureTime, Train Train, string Platform, string? Sector);
 }
